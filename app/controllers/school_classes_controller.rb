10e38c60b12f9ab11019462d13d064b4a54c900e
jrshort89 <jrshort89@gmail.com>
2020-10-28 22:22:12 -0400

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,17 @@
+class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
